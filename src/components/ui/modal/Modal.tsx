d3864f9cb01c36b0e999ff759e5956cb7de44148
leke-osez <leke.osez@gmail.com>
2025-10-29 13:50:35 +0100

    UPDATED version

--- src/components/ui/modal/Modal.tsx
+++ src/components/ui/modal/Modal.tsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+// import { Dialog, Transition } from "@headlessui/react";
 import { Button } from "../buttons/Button";
 import CloseIcon from "../../../assets/comps/CloseIcon";
 import { cn } from "../../../utils";
@@ -9,6 +9,7 @@ import {
   DialogPortal,
   DialogRoot,
 } from "./RadixModal";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export interface ModalCompProps {
   isOpen: boolean;
@@ -35,19 +36,50 @@ export const Modal = ({
 
   return (
     // <Transition.Root show={isOpen} as={Fragment}>
-    <DialogRoot
-      // as="div"
-      // className="relative z-[292929]"
-      // open={isOpen}
-      defaultOpen
-    >
-      <DialogPortal>
-        {/* overlay */}
+    // <DialogRoot
+    //   // as="div"
+    //   // className="relative z-[292929]"
+    //   open={isOpen}
+    //   // defaultOpen
+    // >
+    //   <DialogPortal>
+    //     {/* overlay */}
 
-        <DialogOverlay className="fixed inset-0 bg-[rgba(0,0,0,0.25)] bg-opacity-75 transition-opacity" />
+    //     <DialogOverlay className="fixed inset-0 bg-[rgba(0,0,0,0.25)] bg-opacity-75 transition-opacity" />
 
-        {/* main modal container */}
-        <DialogContent className="fixed inset-0 z-10 overflow-y-auto">
+    //     {/* main modal container */}
+    //     <DialogContent className="fixed inset-0 z-10 overflow-y-auto">
+    //       <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+    //         {/* main modal panel */}
+    //         <div
+    //           className={cn(
+    //             `relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all min-w-[100px] max-w-[99%]  min-h-fit max-h-[90vh] ${
+    //               dialogClassName ? dialogClassName : ""
+    //             }`
+    //           )}
+    //         >
+    //           {showCloseButton && (
+    //             <div className="w-full flex justify-end px-3">
+    //               <Button
+    //                 className="px-1"
+    //                 variant={"plain"}
+    //                 onClick={handleClose}
+    //               >
+    //                 {<CloseIcon />}
+    //               </Button>
+    //             </div>
+    //           )}
+    //           {children}
+    //         </div>
+    //       </div>
+    //     </DialogContent>
+    //   </DialogPortal>
+    // </DialogRoot>
+
+    <Dialog.Root open={isOpen} onOpenChange={close}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="z-10 fixed inset-0 bg-[rgba(0,0,0,0.25)] bg-opacity-75 transition-opacity" />
+        <Dialog.Content className="z-10  fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
             {/* main modal panel */}
             <div
@@ -71,9 +103,10 @@ export const Modal = ({
               {children}
             </div>
           </div>
-        </DialogContent>
-      </DialogPortal>
-    </DialogRoot>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+
     // </Transition.Root>
   );
 };
